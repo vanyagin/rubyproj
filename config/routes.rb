22a3bbcf215612c3b878ef9427eb95a2fa6aece9
AlexDanko <57270154+AlexDanko@users.noreply.github.com>
2020-12-13 15:51:02 +0300

    front_update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get '/home/choose_category/:id', to: 'home#choose_category'
-  get '/home/choose_category/:id/choose_product/:id', to: 'home#choose_product'
+  get '/home/choose_category/home/choose_product/:id', to: 'home#choose_product'
 end
