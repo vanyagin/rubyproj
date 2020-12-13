e8d9c36734abe3fe92b1b6aaf32ecc1ccebc03c0
vanyagin <58004940+vanyagin@users.noreply.github.com>
2020-12-13 15:06:58 +0300

    front_update
    добавил отображение товаров по категориям

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,11 @@ class HomeController <ApplicationController
 
   def choose_category
     @category = Category.find_by_id params[:id]
+    @product = Product.all.select { |p| p.category_id == @category.id }
+  end
+
+  def choose_product
+    @product = Product.find_by_id params[:id]
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,60 +24,60 @@ unless Producer.any?
 end
 
 unless Product.any?
-Product.create!(name: 'Миксер Moulinex Quick Mix HM3108B1',
-                price: 2490,
-                description: 'Ручной миксер Moulinex QuickMix HM3108B1 пригодится он и в приготовлении молочных коктейлей и муссов.',
-                quantity: 13,
-                category_id: c1.id,
-                producer_id: p1.id)
-Product.create!(name: 'Миксер Philips HR3740/00',
-                price: 3690,
-                description: '-',
-                quantity: 15,
-                category_id: c1.id,
-                producer_id: p2.id)
-Product.create!(name: 'Тостер Philips HD2582/90',
-                price: 2990,
-                description: '-',
-                quantity: 10,
-                category_id: c2.id,
-                producer_id: p2.id)
-Product.create!(name: 'Тостер Tefal TT1A1830',
-                price: 3290,
-                description: '-',
-                quantity: 11,
-                category_id: c2.id,
-                producer_id: p5.id)
-Product.create!(name: 'Электромясорубка Redmond RMG-1236',
-                price: 4990,
-                description: '-',
-                quantity: 7,
-                category_id: c3.id,
-                producer_id: p3.id)
-Product.create!(name: 'Электромясорубка Moulinex HV1 ME106832',
-                price: 6990,
-                description: '-',
-                quantity: 9,
-                category_id: c3.id,
-                producer_id: p1.id)
-Product.create!(name: 'Микроволновая печь соло Samsung ME81KRW-2',
-                price: 7990,
-                description: '-',
-                quantity: 5,
-                category_id: c4.id,
-                producer_id: p4.id)
-Product.create!(name: 'Кофеварка капельного типа Philips HD7434/20',
-                price: 2990,
-                description: '-',
-                quantity: 9,
-                category_id: c5.id,
-                producer_id: p2.id)
-Product.create!(name: 'Соковыжималка для цитрусовых Moulinex PC120870',
-                price: 1990,
-                description: '-',
-                quantity: 15,
-                category_id: c6.id,
-                producer_id: p1.id)
+  Product.create!(name: 'Миксер Moulinex Quick Mix HM3108B1',
+                  price: 2490,
+                  description: 'Ручной миксер Moulinex QuickMix HM3108B1 пригодится он и в приготовлении молочных коктейлей и муссов.',
+                  quantity: 13,
+                  category_id: c1.id,
+                  producer_id: p1.id)
+  Product.create!(name: 'Миксер Philips HR3740/00',
+                  price: 3690,
+                  description: '-',
+                  quantity: 15,
+                  category_id: c1.id,
+                  producer_id: p2.id)
+  Product.create!(name: 'Тостер Philips HD2582/90',
+                  price: 2990,
+                  description: '-',
+                  quantity: 10,
+                  category_id: c2.id,
+                  producer_id: p2.id)
+  Product.create!(name: 'Тостер Tefal TT1A1830',
+                  price: 3290,
+                  description: '-',
+                  quantity: 11,
+                  category_id: c2.id,
+                  producer_id: p5.id)
+  Product.create!(name: 'Электромясорубка Redmond RMG-1236',
+                  price: 4990,
+                  description: '-',
+                  quantity: 7,
+                  category_id: c3.id,
+                  producer_id: p3.id)
+  Product.create!(name: 'Электромясорубка Moulinex HV1 ME106832',
+                  price: 6990,
+                  description: '-',
+                  quantity: 9,
+                  category_id: c3.id,
+                  producer_id: p1.id)
+  Product.create!(name: 'Микроволновая печь соло Samsung ME81KRW-2',
+                  price: 7990,
+                  description: '-',
+                  quantity: 5,
+                  category_id: c4.id,
+                  producer_id: p4.id)
+  Product.create!(name: 'Кофеварка капельного типа Philips HD7434/20',
+                  price: 2990,
+                  description: '-',
+                  quantity: 9,
+                  category_id: c5.id,
+                  producer_id: p2.id)
+  Product.create!(name: 'Соковыжималка для цитрусовых Moulinex PC120870',
+                  price: 1990,
+                  description: '-',
+                  quantity: 15,
+                  category_id: c6.id,
+                  producer_id: p1.id)
 end
 
 
